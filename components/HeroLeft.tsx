a0ae15f71182429ba5c73e6c33004a133c49d31a
CodyK-779 <Codyk9627@Gmail.com>
2025-12-23 15:20:44 +0630

    Made Hero banner details responsive

--- components/HeroLeft.tsx
+++ components/HeroLeft.tsx
@@ -1,22 +1,11 @@
 import { heroData } from "@/data/hero-data";
-import { Bookmark, ShipWheel } from "lucide-react";
+import { Bookmark } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import { Button } from "./ui/button";
 
 interface Props {
   showGame: number;
 }
-
-const marginLeft = (logo: string) => {
-  if (logo === "/game_images/ER-logo.png") return "size-60 -mb-12";
-  if (logo === "/game_images/expedition-logo.png") return "size-48";
-  if (logo === "/game_images/arc-logo.png") return "size-56 -mb-8";
-  if (logo === "/game_images/B6-logo.png") return "size-60 -mb-14";
-  if (logo === "/game_images/RE-logo.png") return "size-56 -mb-8";
-
-  return "size-44 -mb-1";
-};
 
 const HeroLeft = ({ showGame }: Props) => {
   return (
@@ -31,9 +20,9 @@ const HeroLeft = ({ showGame }: Props) => {
         />
         <div className="absolute inset-0 bg-gradient-to-r from-black/80 via-black/20 to-transparent" />
       </div>
-      <div className="absolute bottom-10 left-12 z-10">
+      <div className="absolute min-[1014px]:bottom-10 bottom-8 min-[1014px]:left-12 left-10 z-10">
         <div className="flex flex-col">
-          <div className={`relative ${marginLeft(heroData[showGame].logo)}`}>
+          <div className={`relative ${heroData[showGame].style}`}>
             <Image
               src={heroData[showGame].logo}
               alt="Game Logo"
@@ -42,18 +31,20 @@ const HeroLeft = ({ showGame }: Props) => {
             />
           </div>
 
-          <p className="mb-2 text-lg font-bold">{heroData[showGame].award}</p>
-          <p className="mb-9 font-medium max-w-[420px]">
+          <p className="mb-2 min-[1014px]:text-lg text-base font-bold">
+            {heroData[showGame].award}
+          </p>
+          <p className="min-[1014px]:mb-9 mb-7 font-medium max-w-[420px] min-[1014px]:text-base text-sm">
             {heroData[showGame].desc}
           </p>
           <div className="flex items-center gap-4">
             <Link href={heroData[showGame].link}>
-              <button className="nox-btn font-medium px-6 py-3">
+              <button className="nox-btn font-medium px-6 min-[1170px]:py-3 min-[1014px]:py-2.5 py-2 min-[1170px]:text-base min-[800px]:text-sm text-xs">
                 Learn More
               </button>
             </Link>
-            <button className="size-11 bg-white rounded-md flex items-center justify-center">
-              <Bookmark className="size-5 text-neutral-900" />
+            <button className="min-[1170px]:size-11 min-[1014px]:size-10 min-[800px]:size-9 size-8 bg-white rounded-md flex items-center justify-center">
+              <Bookmark className="min-[1170px]:size-5 size-4 text-neutral-900" />
             </button>
           </div>
         </div>
--- data/hero-data.ts
+++ data/hero-data.ts
@@ -4,42 +4,48 @@ export const heroData = [
     logo: '/game_images/ER-logo.png',
     img: "/game_images/ER.jpg",
     award: "Game Of The Year 2022",
-    desc: "Elden Ring Nightreign is a standalone, three-player cooperative survival game where heroes battle through a shifting landscape to defeat powerful bosses across a three-day roguelike loop."
+    desc: "Elden Ring Nightreign is a standalone, three-player cooperative survival game where heroes battle through a shifting landscape to defeat powerful bosses across a three-day roguelike loop.",
+    style: "min-[1014px]:size-60 size-52 min-[850px]:-mb-12 -mb-14"
   },
   {
     link: "/browse/clair-obscur-expedition-33",
     logo: "/game_images/expedition-logo.png",
     img: "/game_images/expedition.jpg",
     award: "Game Of The Year 2025",
-    desc: "Clair Obscur: Expedition 33 is a turn-based RPG set in a Belle Époque-inspired world where a party of survivors must journey to destroy an entity known as 'The Paintress' before she can erase the last of humanity."
+    desc: "Clair Obscur: Expedition 33 is a turn-based RPG set in a Belle Époque-inspired world where a party of survivors must journey to destroy an entity known as 'The Paintress' before she can erase the last of humanity.",
+    style: "min-[1170px]:size-48 min-[1014px]:size-44 min-[900px]:size-40 min-[800px]:size-36 size-32 min-[900px]:mb-0 -mb-2"
   },
   {
     link: "/browse/arc-raiders",
     logo: "/game_images/arc-logo.png",
     img: "/game_images/arc-bg.jpg",
     award: "Best Multiplayer Game 2025",
-    desc: "ARC Raiders is a third-person extraction shooter where players scavenge a lethal, machine-ravaged future Earth while contending with both deadly robots and rival Raiders."
+    desc: "ARC Raiders is a third-person extraction shooter where players scavenge a lethal, machine-ravaged future Earth while contending with both deadly robots and rival Raiders.",
+    style: "min-[1014px]:size-56 min-[900px]:size-48 min-[850px]:size-44 size-40 -mb-8"
   },
   {
     link: "/browse/battlefield-6-2",
     logo: "/game_images/B6-logo.png",
     img: "/game_images/b6.jpeg",
     award: "Best Audio Design 2025",
-    desc: "A modern-day military shooter returning to the series' roots with 64-player matches, a classic four-class system, and large-scale environmental destruction."
+    desc: "A modern-day military shooter returning to the series's roots with 64-player matches, a classic four-class system, and large-scale environmental destruction.",
+    style: "min-[1014px]:size-60 min-[850px]:size-52 min-[800px]:size-48 size-44 -mb-14"
   },
   {
     link: "/browse/resident-evil-9-requiem",
     logo: "/game_images/RE-logo.png",
     img: "/game_images/RE.jpeg",
     award: "Pre Order Available",
-    desc: "A survival horror journey returning to the ruins of Raccoon City, featuring dual campaigns split between stealth-focused newcomer Grace Ashcroft and action-oriented veteran Leon S. Kennedy."
+    desc: "A survival horror journey returning to the ruins of Raccoon City, featuring dual campaigns split between stealth-focused newcomer Grace Ashcroft and action-oriented veteran Leon S. Kennedy.",
+    style: "min-[1014px]:size-56 min-[900px]:size-48 min-[850px]:size-44 size-40 -mb-8"
   },
   {
     link: "/browse/hollow-knight-silksong",
     logo: "/game_images/HK-logo.png",
     img: "/game_images/HK.jpg",
     award: "Best Action/Adventure 2025",
-    desc: "Hollow Knight: Silksong is the Metroidvania sequel where you play as Hornet, an agile princess-protector battling through a vertical kingdom of silk and song to reach a shining citadel."
+    desc: "Hollow Knight: Silksong is the Metroidvania sequel where you play as Hornet, an agile princess-protector battling through a vertical kingdom of silk and song to reach a shining citadel.",
+    style: "min-[1014px]:size-44 min-[900px]:size-40 min-[800px]:size-36 size-32 -mb-1"
   }
 ]
 
