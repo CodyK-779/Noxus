a0bffc1d4a888456eebf41cc9961a9d240504e23
CodyK-779 <Codyk9627@Gmail.com>
2025-12-31 16:50:16 +0630

    Finished New Games Page

--- app/(root)/discover/new-releases/page.tsx
+++ app/(root)/discover/new-releases/page.tsx
@@ -1,3 +1,18 @@
-export default function NewGamesPage() {
-  return <div className="mt-20">new games</div>;
+import { getNewGames } from "@/actions/games-action";
+import NewGamesGrid from "@/components/NewGamesGrid";
+
+export default async function NewGamesPage() {
+  const newGames = await getNewGames();
+
+  return (
+    <main className="max-container mt-32">
+      <h1 className="md:text-4xl min-[400px]:text-3xl text-2xl font-bold">
+        Discover New Releases
+      </h1>
+      <p className="md:text-lg min-[400px]:text-base text-sm font-medium mt-2 text-neutral-300">
+        Check out newly released games in the last 30 days.
+      </p>
+      <NewGamesGrid games={newGames} />
+    </main>
+  );
 }
---
+++ components/NewGamesGrid.tsx
@@ -0,0 +1,89 @@
+import { GamesType } from "@/actions/games-action";
+import { RAWGResponse } from "@/actions/genres-action";
+import Image from "next/image";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
+import { Bookmark } from "lucide-react";
+import {
+  gameRating,
+  platformIconByKey,
+  platformIcons,
+  ratingBadge,
+} from "@/utils/utils";
+import Link from "next/link";
+
+interface Props {
+  games: RAWGResponse<GamesType>;
+}
+
+const NewGamesGrid = ({ games }: Props) => {
+  return (
+    <section className="grid lg:grid-cols-5 md:grid-cols-4 sm:gap-5 min-[400px]:gap-4 gap-3 sm:grid-cols-3 grid-cols-2 pt-16">
+      {games.results.map((game) => (
+        <div key={game.id} className="relative group min-[400px]:mb-10 mb-4">
+          <Link href={`/browse/${game.slug}`}>
+            <div className="relative aspect-[3/4] rounded-md overflow-hidden">
+              {game.background_image ? (
+                <Image
+                  src={game.background_image}
+                  alt={game.name}
+                  fill
+                  sizes="(max-width: 768px) 80vw"
+                  className="object-cover"
+                />
+              ) : (
+                <Image
+                  src="/image-placeholder.webp"
+                  alt="Image placeholder"
+                  fill
+                  sizes="(max-width: 768px) 80vw"
+                  className="object-cover"
+                />
+              )}
+            </div>
+          </Link>
+
+          <Tooltip delayDuration={0}>
+            <TooltipTrigger asChild>
+              <div
+                className={`absolute min-[400px]:top-2.5 min-[400px]:right-2.5 right-2 top-2 sm:hidden group-hover:flex flex items-center justify-center bg-black border border-white p-1 rounded-full cursor-pointer`}
+              >
+                <Bookmark className="min-[400px]:size-3.5 size-3" />
+              </div>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p className="font-semibold">Add to Wishlist</p>
+            </TooltipContent>
+          </Tooltip>
+
+          <p className="mt-2 mb-0.5 font-medium lg:text-sm text-xs text-neutral-400">
+            {new Date(game.released).toLocaleDateString("en-US", {
+              month: "short",
+              year: "numeric",
+            })}
+          </p>
+
+          <p className="lg:text-base text-sm font-bold">{game.name}</p>
+
+          <div className="flex items-center justify-between">
+            {game.platforms && (
+              <div className="flex items-center gap-1">
+                {platformIcons(game.platforms).map((p) => (
+                  <p key={p}>{platformIconByKey(p)}</p>
+                ))}
+              </div>
+            )}
+            <div
+              className={`min-[400px]:px-1.5 px-1 min-[400px]:py-0.5 py:[1.5px] rounded ${ratingBadge(
+                game.rating
+              )} text-white font-semibold min-[400px]:text-xs text-[10px]`}
+            >
+              {gameRating(game.rating)}
+            </div>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
+};
+
+export default NewGamesGrid;
